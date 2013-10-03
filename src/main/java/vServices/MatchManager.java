0fd110f1c6fbf024b9ba754daaa04184092a6d8a
ferplay <dimegliofernando@gmail.com>
2013-10-03 10:46:25 -0300

    MatchManager

--- src/main/java/vServices/MatchManager.java
+++ src/main/java/vServices/MatchManager.java
@@ -1,5 +1,32 @@
 package vServices;
 
-public class MatchManager {
+import java.util.List;
 
+import vClass.ClassDAO;
+import vClass.Game;
+import vClass.MatchDate;
+import vClass.Operation;
+import vClass.SessionManager;
+
+public class MatchManager {
+	
+	@SuppressWarnings("unchecked")
+	public <T extends MatchDate> T executeGet(int match){
+		return (T) SessionManager.runInSession(new AskForMatch(match));
+	}
+	
+	public void executeSave(List<Game> games, int match){
+		SessionManager.runInSession(new SaveMatch(games, match));
+	}
+	
+	public void delete(final int match){
+		SessionManager.runInSession(new Operation<MatchDate>(){
+			
+	      public MatchDate execute(){
+	    	 new ClassDAO<MatchDate>(MatchDate.class).delete(match);
+	    	 return null;
+	      }
+		});
+	}
+	
 }
